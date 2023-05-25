2733025f158f24b3eaa4b294eba4149b7665aa14
tkdcksqkr98 <tom138@naver.com>
2023-05-25 17:48:07 +0900

    FEAT : 주문상세페이지 추가, DB데이터 입력 가능

--- src/main/java/com/multi/withPuppy/shopping/OrderController.java
+++ src/main/java/com/multi/withPuppy/shopping/OrderController.java
@@ -2,62 +2,60 @@ package com.multi.withPuppy.shopping;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller 
+import com.multi.withPuppy.user.UserVO;
+import com.sun.media.sound.ModelAbstractOscillator;
+
+@Controller
 @RequestMapping("shopping")
 public class OrderController {
-	
-	@Autowired    
+
+	@Autowired
 	OrderDAO dao;
-	
+
 	@RequestMapping("insertOr")
-	public String insert(OrderVO bag) {
-		System.out.println("OrderVO : "+bag);
-		dao.insert(bag);
-		return "order";
+	@ResponseBody
+	public int insert(OrderVO bag) {
+		System.out.println("orderVO : " + bag);
+		int result = dao.insert(bag);
+		return result;
 	}
-	
+
+	@RequestMapping("insertDe")
+	@ResponseBody
+	public int insert(Order_detailVO bag) {
+		int order_id = dao.lastId();
+		System.out.println("order_detailVO : " + bag);
+		bag.setOrder_id(order_id);
+		int result = dao.insert(bag);
+		return result;
+	}
+
 	@RequestMapping("delete")
 	public String delete(String title) {
 		dao.delete(title);
 		return "redirect:order";
 	}
+
 	@RequestMapping("update")
 	public void update(OrderVO bag) {
 		dao.update(bag);
 	}
-	
+
 	@RequestMapping("orderList")
-	public void order() {
+	public void order(String user_id, Model model) {
+		// order 테이블 데이터 추가
+		// order_id를 받아서 orderDetail테이블에 값 추가
+		UserVO bag = dao.bringUser(user_id);
+		model.addAttribute("bag", bag);
+		
 	}
 
-	@RequestMapping("insertOr1")
-	public void order1() {
-	}
-	@RequestMapping("deleteOr1")
-	public void order2() {
-	}
-	@RequestMapping("detailOr1")
-	public void order3() {
-	}
-	
-	
-	
-	
-	@RequestMapping("one")
-	public void one(int order_id, Model model) {
-		OrderVO vo = dao.one(order_id);
-		model.addAttribute("vo", vo);
-	}
-	@RequestMapping("list")
-	public String list(Model model) {
-		List<OrderVO> list = dao.list();
-		model.addAttribute("list", list);
-		return "order/list";
-	}
 }
--- src/main/java/com/multi/withPuppy/shopping/OrderVO.java
+++ src/main/java/com/multi/withPuppy/shopping/OrderVO.java
@@ -3,46 +3,121 @@ package com.multi.withPuppy.shopping;
 import java.sql.Timestamp;
 
 public class OrderVO {
+	/* 주문 번호 */
 	private int order_id;
+	/* 주문자 */
 	private String user_id;
-	private Timestamp order_date;
-	private String order_address;
-	private int tot_price;
+	/* 주문자 전화번호 */
+	private String order_date;
+	/* 주문자 주소 */
+	private String receiver_name;
+	/* 수령  배송지명 */
+	private String receiver_phone;
+	/* 수령인 연락처 */
+	private String receiver_id;
+	/* 수령인 주소 */
+	private String addr1;
+	
+	private String addr2;
+	
+	private String addr3;
+
+	private int total_price;
+	
+
 	public int getOrder_id() {
 		return order_id;
 	}
+
 	public void setOrder_id(int order_id) {
 		this.order_id = order_id;
 	}
+
 	public String getUser_id() {
 		return user_id;
 	}
+
 	public void setUser_id(String user_id) {
 		this.user_id = user_id;
 	}
-	public Timestamp getOrder_date() {
+
+	public String getOrder_date() {
 		return order_date;
 	}
-	public void setOrder_date(Timestamp order_date) {
+
+	public void setOrder_date(String order_date) {
 		this.order_date = order_date;
 	}
-	public String getOrder_address() {
-		return order_address;
+
+	public String getReceiver_name() {
+		return receiver_name;
 	}
-	public void setOrder_address(String order_address) {
-		this.order_address = order_address;
+
+	public void setReceiver_name(String receiver_name) {
+		this.receiver_name = receiver_name;
 	}
-	public int getTot_price() {
-		return tot_price;
+
+	public String getReceiver_phone() {
+		return receiver_phone;
 	}
-	public void setTot_price(int tot_price) {
-		this.tot_price = tot_price;
+
+	public void setReceiver_phone(String receiver_phone) {
+		this.receiver_phone = receiver_phone;
 	}
+
+	public String getReceiver_id() {
+		return receiver_id;
+	}
+
+	public void setReceiver_id(String receiver_id) {
+		this.receiver_id = receiver_id;
+	}
+
+	public String getAddr1() {
+		return addr1;
+	}
+
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+
+	public String getAddr2() {
+		return addr2;
+	}
+
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+
+	public String getAddr3() {
+		return addr3;
+	}
+
+	public void setAddr3(String addr3) {
+		this.addr3 = addr3;
+	}
+
+	public int getTotal_price() {
+		return total_price;
+	}
+
+	public void setTotal_price(int total_price) {
+		this.total_price = total_price;
+	}
+
 	@Override
 	public String toString() {
 		return "OrderVO [order_id=" + order_id + ", user_id=" + user_id + ", order_date=" + order_date
-				+ ", order_address=" + order_address + ", tot_price=" + tot_price + "]";
+				+ ", receiver_name=" + receiver_name + ", receiver_phone=" + receiver_phone + ", receiver_id="
+				+ receiver_id + ", addr1=" + addr1 + ", addr2=" + addr2 + ", addr3=" + addr3 + ", total_price="
+				+ total_price + "]";
 	}
+
+	
+
+
+	
+	
 	
 	
 }
